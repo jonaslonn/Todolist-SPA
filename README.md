484b04bfa209fa94cbd7fab437dce0c30f4158fd
Jonas Lönn <jonas_nilsson@live.se>
2020-01-03 17:47:49 +0100

    Initial, first commit, able to POST and GET todos with AJAX and JSON

---
+++ README.md
@@ -0,0 +1,18 @@
+# TODO APP
+
+Singlepage application, development practice with AJAX and JSON.
+
+## API documentation
+
+A single todo must have a title property, and the length should be at least 1 char in length.
+
+**GET /todos** - Returns all the todos
+
+**POST /todos** - Create a new todo. `{title: string}` needs to be provided as JSON
+
+**GET /todos/:id** - Returns a specific todo by id
+
+**PUT /todos/:id** - Update specific todo by id. `{title: string}` needs to be provided as JSON.
+
+**DELETE /todos/:id** - Delete a specific todo by id
+
---
+++ script.js
@@ -0,0 +1,103 @@
+// http://localhost:8091/todos
+// Hämta element
+
+console.log("Welcome to the Todolist");
+
+(function () {
+
+    
+
+    requestList();
+
+    function reqListener() {
+
+        if(!!document.getElementById("ulContent"))
+        {
+            document.getElementById("ulContent").remove();
+            // document.getElementById("ulContent").innerHTML = '';
+        }  
+      
+        const data = JSON.parse(this.responseText);
+        renderList(data.data);    
+    }
+
+    function renderList(data) {
+
+    var ulEl = document.createElement('ul');
+    ulEl.setAttribute('id', 'ulContent')
+    document.body.appendChild(ulEl);
+
+        for(i =0; i < data.length; i++) 
+        {
+            console.log("Name: " + data[i].title);                
+
+            let liEl = document.createElement('li');
+            liEl.textContent = data[i].id + ") " + data[i].title;
+            
+            let btnEl = document.createElement('button');
+            btnEl.setAttribute('data-id', data[i].id);
+            btnEl.addEventListener("click", onClickDelete);
+            btnEl.style = "margin:5px; color:white; background-color:red;";
+            btnEl.textContent = "delete";
+
+
+            ulEl.appendChild(liEl);
+            liEl.appendChild(btnEl);
+
+        }
+    }
+
+    function requestList() {
+
+        const req = new XMLHttpRequest();
+    
+        req.addEventListener("load", reqListener);
+        req.open("GET", "/todos");
+        req.send(); 
+
+    }
+
+
+    // Skapa ny todo
+    const formEl = document.querySelector("form");
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        const postReq = new XMLHttpRequest();
+
+        postReq.open("POST", "/todos");
+        postReq.setRequestHeader("Content-Type", "application/json");
+
+        const titleEl = document.getElementById("title");
+
+        const ob = { title: titleEl.value };
+        postReq.send(JSON.stringify(ob));
+
+        // console.log(titleEl.value);
+        // const ulEl = document.querySelector("ul");
+        
+        requestList();
+    }
+
+    formEl.addEventListener("submit", onSubmit);
+
+
+
+    
+    // Ta bort todo
+    function onClickDelete(e) {
+
+        const delReq = new XMLHttpRequest();
+
+        delReq.open("DELETE", "/todos/" + this.dataset.id);
+        delReq.send();
+
+        requestList();
+    }
+
+
+
+    // put post
+}())
+
